9be94e818db5d0072dedf9bc115a9b15d7291f3b
Ian Macalinao <me@ian.pw>
2014-08-05 16:14:13 -0500

    Add teasing
    Signed-off-by: Ian Macalinao <me@ian.pw>

--- server/routes.js
+++ server/routes.js
@@ -5,15 +5,22 @@
 'use strict';
 
 var errors = require('./components/errors');
+var JsTeaser = require('jsteaser');
+var tease = function(title, text) {
+  return (new JsTeaser({
+    title: title,
+    text: text
+  }));
+};
 
 module.exports = function(app) {
+  app.post('/tease', function(req, res) {
+    res.send(tease(req.body.title, req.body.text));
+  });
 
-  // Insert routes below
-  app.use('/api/things', require('./api/thing'));
-  
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-   .get(errors[404]);
+    .get(errors[404]);
 
   // All other routes should redirect to the index.html
   app.route('/*')
